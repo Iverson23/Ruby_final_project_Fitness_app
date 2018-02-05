788b513d47f74563e12f3a88a50b299b2329aeff
AdamTersl <tersl.adam@seznam.cz>
2018-02-05 16:05:37 +0100

    added rating service

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,5 @@
 class Meal < ApplicationRecord
-
+  ratyrate_rateable 'meal'
   def thumbnail
     "/images/meals/#{meal_type}/#{title}.jpg"
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  post '/rate' => 'rater#create', :as => 'rate'
   resources :meals
   devise_for :users
   resources :workouts
