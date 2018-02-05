8deb80522c1b77af6b875c6413da1a034cbb4cc3
AdamTersl <tersl.adam@seznam.cz>
2018-02-05 13:49:40 +0100

    added users management and modified ask coach functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205075511) do
+ActiveRecord::Schema.define(version: 20180205114550) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "question_id", null: false
@@ -42,7 +42,28 @@ ActiveRecord::Schema.define(version: 20180205075511) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   create_table "workout_compositions", force: :cascade do |t|
+    t.integer "excercise_id"
+    t.integer "workout_id"
+    t.integer "sets"
+    t.integer "reps"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -51,9 +72,9 @@ ActiveRecord::Schema.define(version: 20180205075511) do
     t.string "title"
     t.text "description"
     t.string "workout_type"
+    t.integer "rest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "rest"
   end
 
 end
