3df27b24ce065f56f29df9dfbd5b537813dfef42
AdamTersl <tersl.adam@seznam.cz>
2018-02-06 08:32:31 +0100

    seed production test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,160 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+News.create(title:"Static exercises could help prevent runners' chronic back pain", body:"A new study which examines the causes of chronic back pain in runners has revealed the exercises which could help prevent it.
+
+    Published in the Journal of Biomechanics, the research from The Ohio State University Wexner Medical Center, suggests that it is weak deep core muscles that put runners at higher risk of developing low back pain.
+
+    And, that most people’s deep core muscles are nowhere near as strong as they should be.
+
+    In the study, researchers used motion detection technology and force-measuring floor plates to measure the dimensions of runner’s bodies and how they moved to create a computer model specific to that person.
+
+    This allowed them to examine how every bone moves and how much pressure is put on each joint before using the simulation to virtually 'turn off' certain muscles and observe how the rest of the body compensates.
+
+    Interestingly, they found that weak deep core muscles force superficial muscles like abs to work harder and reach fatigue faster.
+
+    And, when those superficial muscles are doing all the work the deep core should be doing, this is when pain occurs.
+
+    “When your deep core is weak, your body is able to compensate in a way that allows you to essentially run the same way,” said Ajit Chaudhari, associate professor of physical therapy and biomedical engineering at The Ohio State University Wexner Medical Center.
+
+    “But that increases the load on your spine in a way that may lead to low back pain.”
+
+As such, Chaudhari explains that traditional ab exercises like sit-ups or back extensions will not give you the strong core you need to be a better runner.
+
+    Instead, he says exercises like planks that focus on stabilising the core are what’s needed.
+
+    “Working on a six-pack and trying to become a better runner is definitely not the same thing,” Chaidhari said.")
+News.create(title:"Fitness fanatics' top tips and tricks to stop you skipping the gym", body:"Going to bed no later than 9.44pm, telling someone else about your plans - and packing your gym gear two-and-a-half hours in advance are among the best ways to ensure you don’t back out of exercising, a study has found.
+
+    Having three sets of exercise clothes, taking two days off a week and going to the gym first thing in the morning to ‘get it out of the way’ are other tips people swear by to stick with their fitness plans.
+
+    The poll of 2,000 adults found 54 per cent of people reckon they need to exercise more often than they currently do, with a lack of time (44 per cent), motivation (43 per cent) and money (18 per cent) named as the biggest barriers.
+
+    But 37 per cent struggle more during January than at any other time of the year, with 56 per cent putting it down to the dark mornings and evenings making it harder to get going.
+
+    Andy Birch, Product and Fitness Director at health club group Virgin Active, which commissioned the research said: “One of the most important things when it comes to exercising is being motivated – if you aren’t, it can be easy to find any little excuse to talk yourself out of going.
+
+    “We all know January can be the hardest time of year to get yourself motivated thanks to the Christmas hangover, the short, dark days and the cold weather.
+
+    “One of the best ways to do this is to find a workout that works for you. Whether that’s classes, gym, swim or a mixture of all three.
+
+    “When you’re engaged with your workout, you’re more likely to remain committed to keeping fit and being a healthier you.
+
+    “Make 2018 the year you feel magnificent – and your year of no excuses.”
+
+On the back of the findings, Virgin Active has worked with mathematician Dr Andrea Baronchelli from City University of London to create a formula to help people get motivated this January and prevent them from backing out of their gym plans at the last minute.
+
+    The equation factors in the number of gym buddies you have, how far in advance you prepare your fitness gear, the delay between finishing work and working out and even the temperature.
+
+    Dr Andrea Baronchelli said: “Reviewing the data, it was clear that components such as the harsh weather, being more organised and working out with a friend all impacted the British public’s motivation levels.
+
+    “Therefore, we playfully combined them into a formula to create the perfect January.”
+
+The study also found that 55 per cent of gym goers who reckon they are good at sticking with their plans swear by going to bed early to make sure they aren’t too tired.
+
+    They also tell at least one person of their plans so it’s harder to back out and have three different sets of fitness clothes, so they always have something to wear.
+
+    Other tips include leaving two days between sessions and heading to the gym straight from the office, so you don’t have a chance to sit down.
+
+    Thirty-eight per cent also find it easier to exercise with a friend – as it means you’ll be letting them down if you pull out of plans.
+
+    But eight in ten admit to coming up with an excuse to get out of exercising, with being too tired the most popular.
+
+    Others avoid going to the gym because it’s too hot or cold, they have a hangover or have been left to go alone by a friend they usually go with.
+
+        ‘It’s the weekend’, ‘my gym kit is still dirty’ and ‘my battery has died so I can’t listen to music’ are other excuses people use to explain their absence at the gym.
+
+        To help people stick to their fitness goals throughout January Hannah Davies, Master Personal Trainer at Virgin Active Aldersgate, shares her top tips to enhance any workout:
+
+                                                                                                                                                                                  Fit it into your lifestyle: Whether that’s setting your alarm 30 minutes earlier and getting your workout in first thing or using your lunch break to burn off those calories. Make sure you plan it into your day, so you can stick to it. Be time efficient and train smart, not long
+Make it social: Grab a friend and train together, or join a team or group and become part of a community where you can be accountable, be pushed and socialise with like-minded people
+Invest in yourself: Get a trainer that is experienced and will tailor your sessions to you and your goals
+Start with small changes: Setting yourself one thing to change each week is much more manageable and will lead to long term changes without feeling too overwhelming
+Virgin Active is offering twelve-month membership with nothing to pay until February including no joining fee, valid until Wednesday 17th January.
+
+    The #NoExcuses online offer grants new members nationwide access to Virgin Active’s revolutionary gym floor, up to 120 innovative classes per week, pool, steam and sauna facilities.")
+News.create(title:"How often you need to exercise to see results", body:"If you've renewed your commitment to getting fit now that January has arrived, you may be wondering how much time that goal will require.
+
+For your workouts to produce real results, exercise has to be a regular habit, Chris Jordan, the exercise physiologist who came up with the seven-minute workout, told Business Insider.
+
+Jordan's viral routine, officially called the “Johnson & Johnson Official Seven Minute Workout” is based on a popular form of fitness called interval training. It's designed to give you the benefits of a sweaty bike ride or longer cardio workout in just a few minutes - but you have to commit to doing it regularly.
+
+That means working out three-five times per week, at the minimum, Jordan said.
+
+His insight is bolstered by a new study published in January in the American Heart Association's journal Circulation that found that the best results for heart health were gleaned when participants worked out four-five times per week.
+
+    For that study, researchers split 53 adults into two groups, one of which did two years of supervised exercise four to five days per week while the other simply did yoga and balance exercises. At the end of the study, the higher-intensity exercisers saw significant improvements in their heart's performance.
+
+“We found what we believe to be the optimal dose of the right kind of exercise,” Benjamin Levine, the author of the study and a professor of internal medicine at the University of Texas Southwestern, said in a statement.
+
+That advice holds steady whether you're looking for physical results like leaner limbs and toned muscles or psychological ones like improved mood and higher energy levels. Both Jordan and Levine recommend interspersing cardio - running on a treadmill, riding a bike, or doing high-intensity interval training - with resistance training like planks, squats, or leg raises.
+
+    Here's an example five-day training plan you can try that Jordan shared with us:
+
+Monday: Cycling and upper body resistance training, like arm raises
+Tuesday: Yoga and lower body resistance training, like squats
+Wednesday: Running and upper body resistance training, like bench presses
+Thursday: Rest
+Friday: Boxing and lower body resistance training, like leg raises
+Whichever workout you try, however, the most important thing is to keep doing it.
+
+“To achieve results,” Jordan said, “consistency is key.")
+News.create(title:"Sweatiquette: The fitness fanatic’s guide to good gym manners", body:"Anyone who regularly goes to the gym knows that there are certain unspoken rules that everyone abides by.
+
+    Wipe down your machine after use, don’t hover over someone when they’re using a piece of equipment… basically, use your common sense and respect others.
+
+    However, some people at the gym will always falter in the manners department.
+
+    Debrett’s, a recognised authority on etiquette since 1769, has formulated a handy gym etiquette guide so that everyone can work out in harmony.
+
+    Keeping it clean
+There’s nothing worse than going to use a piece of gym equipment, only to find that the person who was using it before you has left without giving it a proper wipe down.
+
+    This is even more gross when you consider the fact that 12 per cent of people don’t wear deodorant at the gym, with 18 per cent of people wearing their exercise clothes more than once without giving them a thorough wash, according to a YouGov study conducted in partnership with Freya Lingerie.
+
+    Almost a fifth of people neglect to wipe down machines after use, failing to consider other gym members who may prefer not to use equipment that’s covered with sweat.
+
+    Debrett’s advises using paper towels to briefly clean the equipment and perhaps even carrying some hygiene spray with you.
+
+    Making friends
+While many people prefer to exercise in solace when they go to the gym, 42 per cent of us would very happily chat with fellow gym goers.
+
+    It’s been revealed that 26 per cent of people aged between 18 and 45 have made new friends while working out.
+
+    However, Debrett’s has advised people tread with caution when attempting to make idle chat.
+
+    If someone is exhibiting clear signals that they don’t want to engage in conversation, for example if they’re listening to music or appear very focussed, it would be best to leave them alone, no matter how friendly your intentions.
+
+    Waiting your turn
+When you go to the gym during peak hours, chances are you may have to wait for others to finish their reps before using particular pieces of equipment.
+
+    However, waiting closely by someone as they’re engrossed in their workout can be one of the most annoying things you can do, according to Debrett’s.
+
+    13 per cent of people would describe someone hovering over them while they’re using a machine as their greatest gym pet peeve.
+
+    On the other hand, it’s important not to use the equipment selfishly.
+
+    Eight per cent of people surveyed admitted that they sometimes sit on exercise machines to chat with friends, while seven per cent of people confessed that they often book classes and decide not to go without cancelling their place, leaving their spot unfilled.
+
+        Avoid being leery
+    You never know where you’re going to find love.
+
+        Only a small number of people go to the gym specifically to seek out romance, with a mere four per cent of Brits admitting that they do so.
+
+        While only 16 per cent of women have said that they wouldn’t mind being asked out at the gym in comparison to 33 per cent of men, there’s no reason why you can’t meet a potential partner between the barbells as long as you’re attentive to other people’s signals.
+
+        As mentioned above, if someone is listening to music or appears content working out without company, it’d be best to leave them alone.
+
+            However, nearly half of Brits believe that it’s acceptable to ask someone out at the gym.
+
+            Try not to be selfie-centred
+        The study discovered that 25 per cent of women who go to their gym use their phone while working out, with 22 per cent of men also unable to resist.
+
+            Furthermore, six per cent of people feel the need to take selfies during their workouts, a habit that unsurprisingly annoys five per cent of fellow gym-goers.
+
+            If you can’t go to the gym without updating your Instagram story, make sure that you don’t accidentally snap any unsuspecting people in the background.
+
+            Understandably, many people would prefer not to be pictured while working up a sweat.")
 News.create(title:"Is walking 10,000 steps a day really good for you", body:"Is walking 10,000 steps a day actually beneficial for your health?
 
                                                                                                                                   While your Fitbit may beep in celebration on your wrist when you hit the 10,000 step benchmark, recent research has shown that the daily goal may not be all it’s cracked up to be.
